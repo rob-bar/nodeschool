90f5432f320135966a47f998218881a52a9b3031
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-01-22 10:07:30 +0100

    lesson 7

--- lesson6.js
+++ lesson6.js
@@ -5,5 +5,5 @@ mymodule(process.argv[2], process.argv[3], function(err, data){
 
   data.forEach(function(file){
     console.log(file);
-  }
+  });
 });
--- mymodule.js
+++ mymodule.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+
 module.exports = function (dir, ext, callback) {
   fs.readdir(dir, function(err, list) {
     if (err)
@@ -7,7 +8,7 @@ module.exports = function (dir, ext, callback) {
 
     list = list.filter(function (file) {
       return path.extname(file) === '.' + ext
-    })
+    });
 
     callback(null, list);
   });
